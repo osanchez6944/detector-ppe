3d3d8e02f7a8a1b8a649fa2d138d720f3d1d034b
osanchez6944 <osanchez694@unab.edu.co>
2026-04-23 23:31:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
 import streamlit as st
+from ultralytics import YOLO
 
-st.title("Prueba de despliegue")
-st.write("Si ves esto, Streamlit funciona correctamente.")
+# ESTO ES LO QUE HACE QUE LA APP CARGUE
+@st.cache_resource
+def load_model():
+    # Asegúrate de que best.pt esté en la raíz del repo
+    return YOLO("best.pt")
+
+st.title("Detector PPE")
+model = load_model() # Solo se carga una vez en memoria
+st.write("Modelo cargado exitosamente.")
